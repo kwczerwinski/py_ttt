3b15125974fb3e2be088a483db95a7b2411dc6eb
kwczerwinski <kwczerwinski@outlook.com>
2023-02-08 21:05:57 +0100

    simple grid

---
+++ main.py
@@ -0,0 +1,11 @@
+# Display grid
+# Handle player moves
+# Track game state
+# Check for win or draw
+# Multiple rounds
+
+def display_grid():
+	print("...\n...\n...")
+
+if __name__ == "__main__":
+	display_grid()
